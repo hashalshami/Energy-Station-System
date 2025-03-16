319b6385c7c07ee6a338dd0e33d4d82ba5456557
hashemSoft <hashalshamii@gmail.com>
2025-03-16 21:04:19 +0300

    first

--- push.bat
+++ push.bat
@@ -2,6 +2,6 @@
 set /p commit_message=Enter Commit Message (Commit Message): 
 git add *
 git commit -m "%commit_message%"
-git branch -M Hashem
-git push origin Hashem
+git branch -M master
+git push origin master
 pause
